50f4e7d2fda96be3ec5a4ec371de966d666571c5
andresryes <enriquebolanos@ufm.edu>
2020-09-28 20:19:53 -0600

    finished setup web app

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Admin-console-web
 Manage web administrator console for the Covid relief - Contact trace app
 
+docker build .
+
+
+
 chmod 770 run.sh
--- app/app.py
+++ app/app.py
@@ -8,5 +8,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    debug=True
+    debug=False
     app.run(host="0.0.0.0",port=80,debug=debug)
